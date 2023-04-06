7d22bf1ce54eca6299bb18369048132183e17eb9
Lucas_Miras-ZirTyz <lucas.miras@live.fr>
2023-04-07 00:34:19 +0200

    Ajout des différents bloc pour la création procédural

---
+++ Assets/Scripts/ProceduralLevelBuilder.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralLevelBuilder : MonoBehaviour
+{
+
+    [Header("Variables")]
+
+    [SerializeField]
+    private float v_NbRooms;
+
+    [SerializeField]
+    private float v_NbCorridors;
+
+    [Header("références")]
+
+    [SerializeField]
+    private GameObject r_Start;
+
+    [SerializeField]
+    private GameObject r_End;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
